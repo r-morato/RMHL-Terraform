22e1bbffea12a8bf031fa2c31d7fa0465ecf64c6
Roger Morato <r.morato.b@gmail.com>
2025-06-21 17:30:46 +0100

    Create main.tf

---
+++ proxmox-lxc/main.tf
@@ -0,0 +1,28 @@
+provider "proxmox" {
+  pm_api_url = var.pm_api_url
+  pm_user    = var.pm_user
+  pm_token   = var.pm_api_token
+  pm_tls_insecure = true
+}
+
+resource "proxmox_lxc" "homelab_container" {
+  count     = length(var.containers)
+  hostname  = var.containers[count.index].hostname
+  target_node = var.proxmox_node
+  ostemplate = var.template_storage + ":" + var.template_path
+  password  = var.container_password
+  cores     = var.containers[count.index].cores
+  memory    = var.containers[count.index].memory
+  netif {
+    name = "eth0"
+    bridge = "vmbr0"
+    ip = var.containers[count.index].ip
+    gw = var.gateway
+  }
+  rootfs {
+    storage = var.rootfs_storage
+    size = "4G"
+  }
+  start = true
+  unprivileged = true
+}
