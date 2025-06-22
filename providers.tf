7663e2537e005cf73a96aaa76f51d9d338a83af0
Roger Morato <r.morato.b@gmail.com>
2025-06-22 17:14:24 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,10 +6,3 @@ terraform {
     }
   }
 }
-
-provider "proxmox" {
-  pm_api_url          = var.pm_api_url
-  pm_api_token_id     = var.pm_user
-  pm_api_token_secret = var.pm_api_token
-  pm_tls_insecure     = true
-}
