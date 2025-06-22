8a1c9ab89f126be6e35e1a9ee347eca063c5f340
Roger Morato <r.morato.b@gmail.com>
2025-06-22 14:55:21 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,10 +1,15 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "telmate/proxmox"   
-      version = "~> 2.9.11"         
+      source  = "telmate/proxmox"
+      version = "~> 2.9.11"
     }
   }
+}
 
-  required_version = ">= 1.2.0"  
+provider "proxmox" {
+  pm_api_url          = var.pm_api_url
+  pm_api_token_id     = var.pm_user
+  pm_api_token_secret = var.pm_api_token
+  pm_tls_insecure     = true
 }
