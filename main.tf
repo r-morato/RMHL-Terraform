e454aef95049ea29d1052138cfd630b8ee763783
Roger Morato <r.morato.b@gmail.com>
2025-06-22 17:55:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "proxmox" {
-  pm_api_url = var.pm_api_url
-  pm_user    = var.pm_user
-  pm_token   = var.pm_api_token
-  pm_tls_insecure = true
+  pm_api_url          = var.pm_api_url
+  pm_api_token_id     = var.pm_user
+  pm_api_token_secret = var.pm_api_token
+  pm_tls_insecure     = true
 }
 
 resource "proxmox_lxc" "homelab_container" {
