507ce94a6ce5b2d9d1b60e7e96f4772517b3346d
Roger Morato <r.morato.b@gmail.com>
2025-06-22 20:51:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "proxmox" {
   pm_api_url          = var.pm_api_url
-  pm_api_token_id     = var.pm_user
-  pm_api_token_secret = var.pm_api_token
+  pm_user             = var.pm_user
+  pm_password         = var.pm_api_token
   pm_tls_insecure     = true
 }
 
