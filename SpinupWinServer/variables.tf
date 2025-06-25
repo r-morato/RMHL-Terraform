b500b1c118ae9ed79040da47ac45af78ceb83ba1
Roger Morato <r.morato.b@gmail.com>
2025-06-25 22:20:43 +0100

    Update variables.tf

--- SpinupWinServer/variables.tf
+++ SpinupWinServer/variables.tf
@@ -2,7 +2,6 @@ variable "pm_api_url" {}
 variable "pm_user" {}
 variable "pm_api_token" {}
 
-variable "container_password" {}
 variable "gateway" {}
 variable "template_storage" {}
 variable "rootfs_storage" {}
