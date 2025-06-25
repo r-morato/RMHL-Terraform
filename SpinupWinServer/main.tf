f097a8d27ab02c00782ff489091a8d821de80527
Roger Morato <r.morato.b@gmail.com>
2025-06-25 22:03:49 +0100

    Update main.tf

--- SpinupWinServer/main.tf
+++ SpinupWinServer/main.tf
@@ -5,7 +5,7 @@ provider "proxmox" {
   pm_tls_insecure     = true
 }
 
-resource "proxmox_vm" "homelab_vm" {
+resource "proxmox_vm_qemu" "homelab_vm" {
   target_node = var.proxmox_node
   clone     = 110
   rootfs {
